bc575abccd67c8e94d1d7a34a851710f3a2490fa
zyfccc <zyfccc@126.com>
2018-02-16 15:32:16 +0000

    added comment widget

--- lib/tpl/dokuwiki/tpl_footer.php
+++ lib/tpl/dokuwiki/tpl_footer.php
@@ -8,6 +8,7 @@ if (!defined('DOKU_INC')) die();
 ?>
 
 <!-- ********** FOOTER ********** -->
+<!--
 <div id="dokuwiki__footer"><div class="pad">
     <?php tpl_license(''); // license text ?>
 
@@ -27,7 +28,8 @@ if (!defined('DOKU_INC')) die();
         <a href="http://dokuwiki.org/" title="Driven by DokuWiki" <?php echo $target?>><img
             src="<?php echo tpl_basedir(); ?>images/button-dw.png" width="80" height="15" alt="Driven by DokuWiki" /></a>
     </div>
-</div></div><!-- /footer -->
+</div></div> -->
+<!-- /footer -->
 
 <?php
 tpl_includeFile('footer.html');
